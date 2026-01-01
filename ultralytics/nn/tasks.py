db8189f6ebd29ec0db5c22e6faea70b8972878f8
iswanto <is.san86@gmail.com>
2026-01-01 16:09:28 +0700

    add 6 new blocks

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1047,6 +1047,10 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             ARF,
             SOP,
             FA,
+            HRDE,
+            MDA,
+            DSOB,
+            EAE,
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
